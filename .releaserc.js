dd3b9da4ee9ffbf9f62a72c8b3e3f86f654d863b
David LJ <mail@davidlj95.com>
2023-11-23 13:35:03 +0100

    build: add semantic release (#18)
    * build: add semantic release
    
    * chore: add comments to release config
    
    * ci: publish dist & use it in release
    
    * ci: use makefile + pnpm for release cmd

---
+++ .releaserc.js
@@ -0,0 +1,77 @@
+/**
+ * @type {import('semantic-release').GlobalConfig}
+ */
+module.exports = {
+  branches: [
+    //👇 Fake "release" branch so that we can release alpha versions in `main`
+    //   until we can release 1.0.0
+    'release',
+    { name: 'main', prerelease: 'alpha' },
+  ],
+  plugins: [
+    '@semantic-release/commit-analyzer',
+    '@semantic-release/release-notes-generator',
+    //👇 Publish built version
+    ['@semantic-release/npm', { pkgRoot: './dist/ngx-metadata' }],
+    '@semantic-release/github',
+  ],
+  //👇 Add library name in tag
+  tagFormat: 'ngx-metadata-v${version}',
+  preset: 'conventionalcommits',
+  presetConfig: {
+    header: 'CHANGELOG',
+    types: [
+      {
+        type: 'feat',
+        section: 'Features',
+      },
+      {
+        type: 'fix',
+        section: 'Bug Fixes',
+      },
+      {
+        type: 'perf',
+        section: 'Performance Improvements',
+      },
+      {
+        type: 'revert',
+        section: 'Reverts',
+      },
+      {
+        type: 'docs',
+        section: 'Documentation',
+      },
+      {
+        type: 'style',
+        section: 'Style',
+      },
+      {
+        type: 'chore',
+        section: 'Miscellaneous Chores',
+      },
+      {
+        type: 'refactor',
+        section: 'Code Refactoring',
+      },
+      {
+        type: 'test',
+        section: 'Tests',
+      },
+      {
+        type: 'build',
+        section: 'Build System',
+      },
+      {
+        type: 'ci',
+        section: 'Continuous (Integration|Deployment)',
+      },
+    ],
+  },
+  writerOpts: {
+    //👇 Add library name in release notes
+    // https://github.com/conventional-changelog/conventional-changelog/tree/conventional-changelog-writer-v7.0.1/packages/conventional-changelog-writer#finalizecontext
+    finalizeContext: (context) => {
+      return { ...context, version: `\`ngx-metadata\` v${context.version}` }
+    },
+  },
+}

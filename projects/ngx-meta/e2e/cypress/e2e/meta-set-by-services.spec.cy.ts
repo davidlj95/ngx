ec38f5be35a9e17989331bd3a8c24a893c0e4843
David LJ <mail@davidlj95.com>
2023-11-29 03:25:38 +0100

    test(e2e): add Open Graph services test (#50)

--- projects/ngx-meta/e2e/cypress/e2e/meta-set-by-services.spec.cy.ts
+++ projects/ngx-meta/e2e/cypress/e2e/meta-set-by-services.spec.cy.ts
@@ -1,8 +1,9 @@
 import generalMetadata from '../fixtures/general-metadata.json'
+import openGraphSpecific from '../fixtures/open-graph-specific.json'
 import { META_SET_BY_SERVICES } from '../fixtures/routes'
 
 describe('Meta set by services', () => {
-  before(() => {
+  beforeEach(() => {
     cy.visit(META_SET_BY_SERVICES)
   })
 
@@ -31,5 +32,52 @@ describe('Meta set by services', () => {
         .should('have.attr', 'lang')
         .and('eq', generalMetadata.locale)
     })
+  })
+
+  it('sets all Open Graph metadata', () => {
+    // Set from general metadata
+    cy.fixture('general-metadata.json').then((meta: typeof generalMetadata) => {
+      cy.get('meta[property="og:title"]')
+        .should('have.attr', 'content')
+        .and('eq', meta.title)
+      cy.get('meta[property="og:image"]')
+        .should('have.attr', 'content')
+        .and('eq', meta.image.url)
+      cy.get('meta[property="og:image:alt"]')
+        .should('have.attr', 'content')
+        .and('eq', meta.image.alt)
+      cy.get('meta[property="og:url"]')
+        .should('have.attr', 'content')
+        .and('eq', meta.canonicalUrl)
+      cy.get('meta[property="og:description"]')
+        .should('have.attr', 'content')
+        .and('eq', meta.description)
+      cy.get('meta[property="og:locale"]')
+        .should('have.attr', 'content')
+        .and('eq', meta.locale)
+      cy.get('meta[property="og:site_name"]')
+        .should('have.attr', 'content')
+        .and('eq', meta.applicationName)
+    })
+    // Open Graph specifics
+    cy.fixture('open-graph-specific.json').then(
+      (meta: typeof openGraphSpecific) => {
+        cy.get('meta[property="og:type"]')
+          .should('have.attr', 'content')
+          .and('eq', meta.type)
+        cy.get('meta[property="og:image:secure_url"]')
+          .should('have.attr', 'content')
+          .and('eq', meta.image.secureUrl)
+        cy.get('meta[property="og:image:type"]')
+          .should('have.attr', 'content')
+          .and('eq', meta.image.type)
+        cy.get('meta[property="og:image:width"]')
+          .should('have.attr', 'content')
+          .and('eq', meta.image.width.toString())
+        cy.get('meta[property="og:image:height"]')
+          .should('have.attr', 'content')
+          .and('eq', meta.image.height.toString())
+      },
+    )
   })
 })

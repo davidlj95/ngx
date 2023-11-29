08b5f04fd65d07b3f19abd0ebbb935dd19bddf58
David LJ <mail@davidlj95.com>
2023-11-29 01:42:19 +0100

    fix: move general md appliers to providers (#41)
    * fix: move general md appliers to providers
    
    * test(e2e): import module in standalone component

--- projects/ngx-meta/e2e/a17/src/app/app.config.ts
+++ projects/ngx-meta/e2e/a17/src/app/app.config.ts
@@ -1,13 +1,8 @@
-import { ApplicationConfig, importProvidersFrom } from '@angular/core'
+import { ApplicationConfig } from '@angular/core'
 import { provideRouter } from '@angular/router'
 
 import { routes } from './app.routes'
-import { NgxMetaCommonModule } from '@davidlj95/ngx-meta/common'
-import { NgxMetaGeneralModule } from '@davidlj95/ngx-meta/general-metadata'
 
 export const appConfig: ApplicationConfig = {
-  providers: [
-    provideRouter(routes),
-    importProvidersFrom(NgxMetaCommonModule, NgxMetaGeneralModule.forRoot()),
-  ],
+  providers: [provideRouter(routes)],
 }

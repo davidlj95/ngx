47bd795e32279923de0b3f311622c28dda94d4b8
David LJ <mail@davidlj95.com>
2023-11-29 02:04:52 +0100

    test(e2e): rename general metadata json fixture (#44)

--- projects/ngx-meta/e2e/cypress/e2e/meta-set-by-services.spec.cy.ts
+++ projects/ngx-meta/e2e/cypress/e2e/meta-set-by-services.spec.cy.ts
@@ -1,4 +1,4 @@
-import generalMetaSetByService from '../fixtures/general-meta-set-by-service.json'
+import generalMetadata from '../fixtures/general-metadata.json'
 
 describe('Meta set by services', () => {
   before(() => {
@@ -6,31 +6,29 @@ describe('Meta set by services', () => {
   })
 
   it('sets all general metadata', () => {
-    cy.fixture('general-meta-set-by-service.json').then(
-      (meta: typeof generalMetaSetByService) => {
-        cy.title().should('eq', meta.title)
-        cy.get('meta[name="description"]')
-          .should('have.attr', 'content')
-          .and('eq', meta.description)
-        cy.get('meta[name="author"]')
-          .should('have.attr', 'content')
-          .and('eq', meta.author)
-        cy.get('meta[name="keywords"]')
-          .should('have.attr', 'content')
-          .and('eq', meta.keywords.join(','))
-        cy.get('meta[name="generator"]')
-          .should('have.attr', 'content')
-          .and('contain', 'Angular v')
-        cy.get('meta[name="application-name"]')
-          .should('have.attr', 'content')
-          .and('contain', meta.applicationName)
-        cy.get('link[rel="canonical"]')
-          .should('have.attr', 'href')
-          .and('contain', meta.canonicalUrl)
-        cy.get('html')
-          .should('have.attr', 'lang')
-          .and('eq', generalMetaSetByService.locale)
-      },
-    )
+    cy.fixture('general-metadata.json').then((meta: typeof generalMetadata) => {
+      cy.title().should('eq', meta.title)
+      cy.get('meta[name="description"]')
+        .should('have.attr', 'content')
+        .and('eq', meta.description)
+      cy.get('meta[name="author"]')
+        .should('have.attr', 'content')
+        .and('eq', meta.author)
+      cy.get('meta[name="keywords"]')
+        .should('have.attr', 'content')
+        .and('eq', meta.keywords.join(','))
+      cy.get('meta[name="generator"]')
+        .should('have.attr', 'content')
+        .and('contain', 'Angular v')
+      cy.get('meta[name="application-name"]')
+        .should('have.attr', 'content')
+        .and('contain', meta.applicationName)
+      cy.get('link[rel="canonical"]')
+        .should('have.attr', 'href')
+        .and('contain', meta.canonicalUrl)
+      cy.get('html')
+        .should('have.attr', 'lang')
+        .and('eq', generalMetadata.locale)
+    })
   })
 })

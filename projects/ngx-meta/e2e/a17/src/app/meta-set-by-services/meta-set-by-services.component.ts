b65383aca5fe36431ffafd53f6e8510f9a8157dc
David LJ <mail@davidlj95.com>
2023-11-29 01:19:56 +0100

    test(e2e): import module in standalone component

--- projects/ngx-meta/e2e/a17/src/app/meta-set-by-services/meta-set-by-services.component.ts
+++ projects/ngx-meta/e2e/a17/src/app/meta-set-by-services/meta-set-by-services.component.ts
@@ -1,16 +1,22 @@
 import { Component, OnInit } from '@angular/core'
-import { GeneralMetadataService } from '@davidlj95/ngx-meta/general-metadata'
+import {
+  GeneralMetadataService,
+  NgxMetaGeneralModule,
+} from '@davidlj95/ngx-meta/general-metadata'
 import generalMetaSetByService from '../../../../cypress/fixtures/general-meta-set-by-service.json'
 import { JsonPipe } from '@angular/common'
+import { NgxMetaCommonModule } from '@davidlj95/ngx-meta/common'
 
 @Component({
   selector: 'app-meta-set-by-services',
   standalone: true,
   templateUrl: './meta-set-by-services.component.html',
   styleUrl: './meta-set-by-services.component.css',
-  imports: [JsonPipe],
+  imports: [JsonPipe, NgxMetaCommonModule, NgxMetaGeneralModule],
 })
 export class MetaSetByServicesComponent implements OnInit {
+  protected readonly generalMetaSetByService = generalMetaSetByService
+
   constructor(
     private readonly generalMetadataService: GeneralMetadataService,
   ) {}
@@ -18,6 +24,4 @@ export class MetaSetByServicesComponent implements OnInit {
   ngOnInit(): void {
     this.generalMetadataService.apply(generalMetaSetByService)
   }
-
-  protected readonly generalMetaSetByService = generalMetaSetByService
 }

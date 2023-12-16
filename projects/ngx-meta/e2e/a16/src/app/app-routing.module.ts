cab4df6121de5c4bcaf054954bd9949a8a2b4cfd
David LJ <mail@davidlj95.com>
2023-12-16 05:56:32 +0100

    test(e2e): configure A16 app to use ngx-meta

--- projects/ngx-meta/e2e/a16/src/app/app-routing.module.ts
+++ projects/ngx-meta/e2e/a16/src/app/app-routing.module.ts
@@ -1,7 +1,24 @@
 import { NgModule } from '@angular/core'
 import { RouterModule, Routes } from '@angular/router'
+import { ROUTES } from '../../../cypress/fixtures/routes'
+import { MetaSetByServiceComponent } from './meta-set-by-service/meta-set-by-service.component'
+import { MetaSetByRouteComponent } from './meta-set-by-route/meta-set-by-route.component'
+import { MetadataRouteData } from '@davidlj95/ngx-meta/routing'
+import * as METADATA from '../../../cypress/fixtures/metadata.json'
 
-const routes: Routes = []
+const metadataRouteData: MetadataRouteData = { meta: METADATA }
+
+const routes: Routes = [
+  {
+    path: ROUTES.metaSetByService.path,
+    component: MetaSetByServiceComponent,
+  },
+  {
+    path: ROUTES.metaSetByRoute.path,
+    component: MetaSetByRouteComponent,
+    data: metadataRouteData,
+  },
+]
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],

b69c6a47de81fa2a1ad634cd0c724a26900d2f0f
David LJ <mail@davidlj95.com>
2023-12-16 21:30:46 +0100

    test(e2e): use ngx-meta in a15 app

--- projects/ngx-meta/e2e/a15/src/app/app.component.ts
+++ projects/ngx-meta/e2e/a15/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core'
+import { ROUTES } from '../../../cypress/fixtures/routes'
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,8 @@ import { Component } from '@angular/core'
 })
 export class AppComponent {
   title = 'a15'
+  protected readonly navItems = Object.values(ROUTES).map((route) => ({
+    ...route,
+    path: `/${route.path}`,
+  }))
 }

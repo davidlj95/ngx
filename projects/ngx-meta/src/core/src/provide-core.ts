5f772142f9a8ff1246b2ba70a2b03538a5502214
David LJ <mail@davidlj95.com>
2023-12-20 20:05:30 +0100

    chore: refer to Angular inspiration source

--- projects/ngx-meta/src/core/src/provide-core.ts
+++ projects/ngx-meta/src/core/src/provide-core.ts
@@ -7,17 +7,24 @@ import { MetadataValues } from './metadata-values'
 import { DEFAULTS_TOKEN } from './defaults-token'
 
 export function provideCore(
-  ...features: ReadonlyArray<CoreFeature<CoreFeatureKind>>
+  ...features: ReadonlyArray<CoreFeature>
 ): EnvironmentProviders {
   return makeEnvironmentProviders([
     ...features.map((feature) => feature._providers),
   ])
 }
 
+/**
+ * Inspired from Angular router
+ *
+ * https://github.com/angular/angular/blob/17.0.7/packages/router/src/provide_router.ts#L80-L96
+ */
 export const enum CoreFeatureKind {
   Defaults,
 }
-export interface CoreFeature<FeatureKind extends CoreFeatureKind> {
+export interface CoreFeature<
+  FeatureKind extends CoreFeatureKind = CoreFeatureKind,
+> {
   _kind: FeatureKind
   _providers: Provider[]
 }

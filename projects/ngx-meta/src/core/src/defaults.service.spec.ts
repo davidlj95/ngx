983d8c494892510b07d20dced9fa33c7bbdfc8fc
David LJ <mail@davidlj95.com>
2023-12-25 04:57:00 +0100

    refactor: decouple value resolve from defaults service (#117)

--- projects/ngx-meta/src/core/src/defaults.service.spec.ts
+++ projects/ngx-meta/src/core/src/defaults.service.spec.ts
@@ -7,64 +7,38 @@ import { Provider } from '@angular/core'
 import { MetadataValueFromValues } from './metadata-value-from-values'
 import { enableAutoSpy } from '../../__tests__/enable-auto-spy'
 import { MetadataValues } from './metadata-values'
-import { makeGlobalMetadataDefinition } from './__tests__/make-global-metadata-definition'
 
 describe('DefaultsService', () => {
   enableAutoSpy()
 
   describe('get', () => {
-    const dummyDefinition = makeGlobalMetadataDefinition()
-
     describe('when no defaults available', () => {
       describe('like when defaults not injected', () => {
-        it('should return undefined', () => {
+        it('should return empty object', () => {
           const sut = makeSut()
 
-          expect(sut.get(dummyDefinition)).toBeUndefined()
-        })
-      })
-
-      describe('like when empty defaults', () => {
-        const defaults = {}
-
-        it('should return undefined', () => {
-          const sut = makeSut({ defaults })
-
-          expect(sut.get(dummyDefinition)).toBeUndefined()
+          expect(sut.get()).toEqual({})
         })
       })
 
       describe('like when non defined defaults', () => {
         const defaults = undefined
 
-        it('should return undefined', () => {
+        it('should return empty object', () => {
           const sut = makeSut({ defaults })
 
-          expect(sut.get(dummyDefinition)).toBeUndefined()
+          expect(sut.get()).toEqual({})
         })
       })
     })
 
     describe('when defaults available', () => {
       const defaults = { foo: 'bar' }
-      let sut: DefaultsService
 
-      beforeEach(() => {
-        sut = makeSut({ defaults })
-      })
+      it('should return default values', () => {
+        const sut = makeSut({ defaults })
 
-      it('should return default value using collaborator', () => {
-        const value = 'value'
-        const valueFromValues = TestBed.inject(
-          MetadataValueFromValues,
-        ) as jasmine.SpyObj<MetadataValueFromValues>
-        valueFromValues.get.and.returnValue(value)
-
-        expect(sut.get(dummyDefinition)).toEqual(value)
-        expect(valueFromValues.get).toHaveBeenCalledOnceWith(
-          dummyDefinition,
-          defaults,
-        )
+        expect(sut.get()).toEqual(defaults)
       })
     })
   })

97f3f0a11c16f28351db3e7703e3e8dfc055e40c
David LJ <mail@davidlj95.com>
2024-01-18 18:36:27 +0100

    chore: add readonly to metadata provider set

--- projects/ngx-meta/src/core/src/metadata-provider.ts
+++ projects/ngx-meta/src/core/src/metadata-provider.ts
@@ -4,5 +4,5 @@ export type MetadataSetter<T> = (value: T) => void
 
 export abstract class MetadataProvider<Value, Global extends string = string> {
   abstract readonly metadata: Metadata<Global>
-  abstract set: MetadataSetter<Value>
+  abstract readonly set: MetadataSetter<Value>
 }

b737183e33a5c32b842f3ea349588e59f2efc7df
David LJ <mail@davidlj95.com>
2024-01-18 17:55:54 +0100

    refactor: merge Open Graph profile module into parent (#197)

--- projects/ngx-meta/src/open-graph-profile/src/open-graph-profile-metadata.ts
+++
@@ -1,8 +0,0 @@
-import { OpenGraph } from '@davidlj95/ngx-meta/open-graph'
-import { OpenGraphProfile } from './open-graph-profile'
-
-export interface OpenGraphProfileMetadata {
-  openGraph: OpenGraph & {
-    profile: OpenGraphProfile
-  }
-}

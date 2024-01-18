7fb5ae14ecee294e1d8819903dd38029b1d3b944
David LJ <mail@davidlj95.com>
2024-01-18 20:01:28 +0100

    perf: is object into arrow function (#204)

--- projects/ngx-meta/src/core/src/is-object.ts
+++ projects/ngx-meta/src/core/src/is-object.ts
@@ -1,4 +1,3 @@
 // https://stackoverflow.com/a/8511350/3263250
-export function isObject(object: unknown): object is object {
-  return object !== null && typeof object === 'object' && !Array.isArray(object)
-}
+export const isObject = (object: unknown): object is object =>
+  object !== null && typeof object === 'object' && !Array.isArray(object)

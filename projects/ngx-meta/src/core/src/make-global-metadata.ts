9b0ab104de062ed0884475d370784b7543673c9d
David LJ <mail@davidlj95.com>
2024-01-18 18:21:42 +0100

    refactor: remove make global metadata API (#199)

--- projects/ngx-meta/src/core/src/make-global-metadata.ts
+++
@@ -1,6 +0,0 @@
-import { Metadata } from './metadata'
-import { makeMetadata } from './make-metadata'
-
-export const makeGlobalMetadata = <Global extends string = string>(
-  global: Global,
-): Metadata => makeMetadata([global])

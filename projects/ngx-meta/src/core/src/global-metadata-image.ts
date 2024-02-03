f624484067bef5300810c165ea750cd3e7740300
David LJ <mail@davidlj95.com>
2024-02-03 18:22:32 +0100

    chore: some Twitter Card renamings

--- projects/ngx-meta/src/core/src/global-metadata-image.ts
+++ projects/ngx-meta/src/core/src/global-metadata-image.ts
@@ -1,7 +1,7 @@
 /**
  * Defines an image to be used in metadata. Specifically, as:
  *  - Open Graph image
- *  - Twitter card image
+ *  - Twitter Card image
  *
  * @see https://ogp.me/#structured:~:text=The-,og%3Aimage
  * @see https://developer.twitter.com/en/docs/twitter-for-websites/cards/overview/abouts-cards
@@ -12,7 +12,7 @@ export interface GlobalMetadataImage {
    *
    * Used for:
    *  - Open Graph `og:image`/`og:image:url`
-   *  - Twitter card `twitter:image`
+   *  - Twitter Card `twitter:image`
    *
    * @see https://ogp.me/#structured:~:text=og%3Aimage%3Aurl
    * @see https://developer.twitter.com/en/docs/twitter-for-websites/cards/overview/summary-card-with-large-image#:~:text=twitter%3Aimage,-A%20URL%20to
@@ -25,7 +25,7 @@ export interface GlobalMetadataImage {
    *
    * Used for:
    *  - Open Graph `og:image:alt`
-   *  - Twitter card `twitter:image:alt`
+   *  - Twitter Card `twitter:image:alt`
    *
    * @see https://ogp.me/#structured:~:text=og%3Aimage%3Aalt
    * @see https://developer.twitter.com/en/docs/twitter-for-websites/cards/overview/summary-card-with-large-image#:~:text=twitter%3Aimage%3Aalt,-A%20text%20description

05fbcf5d826b3b04964b816c0979976d28f40086
David LJ <mail@davidlj95.com>
2024-03-10 02:27:14 +0100

    fix: use dts-bundle-generator for rollup ts files (#414)
    * fix: use dts-bundle-generator to generate bundled ts files
    
    * fix: use dist dir instead of out dir

---
+++ postbuild.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env sh
+# Commands to run after building
+set -eu
+
+# ngx-meta Typescript definitions rollup
+echo "ℹ️ Bundling Typescript definitions"
+dts-bundle-generator --config dts-config.js
+
+# Replace `index.d.ts` with bundled definitions
+echo "ℹ️ Replacing Typescript index files with bundled definitions"
+NGX_META_DIST_DIR="projects/ngx-meta/dist"
+for dist_file_entry in "$NGX_META_DIST_DIR"/*; do
+  BUNDLED_DEFINITIONS_FILENAME="bundled.d.ts"
+  INDEX_DEFINITIONS_FILENAME="index.d.ts"
+  bundled_definitions_file="$dist_file_entry/$BUNDLED_DEFINITIONS_FILENAME"
+  index_definitions_file="$dist_file_entry/$INDEX_DEFINITIONS_FILENAME"
+  if [ -f "$bundled_definitions_file" ]; then
+    echo "- $bundled_definitions_file -> $index_definitions_file"
+    mv "$bundled_definitions_file" "$index_definitions_file"
+  fi
+done
+
+# Remove Typescript source directories
+echo "ℹ️ Removing Typescript source directories"
+for dist_file_entry in "$NGX_META_DIST_DIR"/*; do
+  entrypoint_src_dir="$dist_file_entry/src"
+  if [ -d "$entrypoint_src_dir" ]; then
+    echo "- $entrypoint_src_dir"
+    rm -rf "$entrypoint_src_dir"
+  fi
+done

1d0392a24d3a66fb374d4833e95dddde6c4f8d96
David LJ <mail@davidlj95.com>
2024-04-10 00:50:15 +0200

    build(e2e): use default imports for JSON around

--- projects/ngx-meta/e2e/apps/a16/src/app/app-routing.module.ts
+++ projects/ngx-meta/e2e/apps/a16/src/app/app-routing.module.ts
@@ -5,7 +5,7 @@ import { AllMetaSetByServiceComponent } from './all-meta-set-by-service/all-meta
 import { AllMetaSetByRouteComponent } from './all-meta-set-by-route/all-meta-set-by-route.component'
 import { MetaSetByRouteAndServiceComponent } from './meta-set-by-route-and-service/meta-set-by-route-and-service.component'
 import { NgxMetaRouteData } from '@davidlj95/ngx-meta/routing'
-import * as ALL_METADATA_JSON from '../../../../cypress/fixtures/all-metadata.json'
+import ALL_METADATA_JSON from '../../../../cypress/fixtures/all-metadata.json'
 import { MetaLateLoadedModule } from './meta-late-loaded/meta-late-loaded.module'
 import { OneMetaSetByServiceComponent } from './one-meta-set-by-service/one-meta-set-by-service.component'
 
--- projects/ngx-meta/e2e/apps/a16/src/app/app.module.ts
+++ projects/ngx-meta/e2e/apps/a16/src/app/app.module.ts
@@ -9,7 +9,7 @@ import { AllMetaSetByServiceComponent } from './all-meta-set-by-service/all-meta
 import { AllMetaSetByRouteComponent } from './all-meta-set-by-route/all-meta-set-by-route.component'
 import { MetaSetByRouteAndServiceComponent } from './meta-set-by-route-and-service/meta-set-by-route-and-service.component'
 import { NgxMetaCoreModule } from '@davidlj95/ngx-meta/core'
-import * as DEFAULTS_JSON from '../../../../cypress/fixtures/defaults.json'
+import DEFAULTS_JSON from '../../../../cypress/fixtures/defaults.json'
 import { NgxMetaRoutingModule } from '@davidlj95/ngx-meta/routing'
 import { NgxMetaStandardModule } from '@davidlj95/ngx-meta/standard'
 import {

7c8cd18cd9e5526822a846133cc72bca0c659d9e
David LJ <mail@davidlj95.com>
2024-04-22 17:57:31 +0200

    build(e2e): use JSON to share service overrides (#563)

--- projects/ngx-meta/example-apps/templates/standalone/src/app/meta-set-by-route-and-service/meta-set-by-route-and-service.component.ts
+++ projects/ngx-meta/example-apps/templates/standalone/src/app/meta-set-by-route-and-service/meta-set-by-route-and-service.component.ts
@@ -2,15 +2,7 @@ import { Component, OnInit } from '@angular/core'
 import { NgxMetaService } from '@davidlj95/ngx-meta/core'
 import { JsonPipe } from '@angular/common'
 import { ActivatedRoute } from '@angular/router'
-import {
-  OPEN_GRAPH_PROFILE_GENDER_FEMALE,
-  OPEN_GRAPH_TYPE_BOOK,
-  OpenGraphMetadata,
-} from '@davidlj95/ngx-meta/open-graph'
-import {
-  TWITTER_CARD_TYPE_SUMMARY_LARGE_IMAGE,
-  TwitterCardMetadata,
-} from '@davidlj95/ngx-meta/twitter-card'
+import ROUTE_SERVICE_OVERRIDES_JSON from '@/e2e/cypress/fixtures/route-service-overrides.json'
 
 @Component({
   selector: 'app-meta-set-by-route-and-service',
@@ -20,18 +12,7 @@ import {
 })
 export class MetaSetByRouteAndServiceComponent implements OnInit {
   protected readonly routeData: unknown
-  protected readonly overriddenMetadata: OpenGraphMetadata &
-    TwitterCardMetadata = {
-    openGraph: {
-      type: OPEN_GRAPH_TYPE_BOOK,
-      profile: {
-        gender: OPEN_GRAPH_PROFILE_GENDER_FEMALE,
-      },
-    },
-    twitterCard: {
-      card: TWITTER_CARD_TYPE_SUMMARY_LARGE_IMAGE,
-    },
-  } as const
+  protected readonly overriddenMetadata = ROUTE_SERVICE_OVERRIDES_JSON
 
   constructor(
     activatedRoute: ActivatedRoute,

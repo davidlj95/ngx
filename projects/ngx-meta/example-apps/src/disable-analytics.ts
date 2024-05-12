bbb241c857a702d4656028c45c001b2c4adbfc34
David LJ <mail@davidlj95.com>
2024-05-12 17:42:34 +0200

    refactor: add execa wrapper (#602)

--- projects/ngx-meta/example-apps/src/disable-analytics.ts
+++ projects/ngx-meta/example-apps/src/disable-analytics.ts
@@ -1,20 +1,12 @@
 import { Log } from './utils.js'
-import { execa } from 'execa'
+import { execa } from './execa.js'
 
 export async function disableAnalytics(opts: {
   cliBinary: string
   appDir: string
 }) {
   Log.step('Disabling Angular analytics')
-  const disableAnalyticsCommand = execa(
-    opts.cliBinary,
-    ['config', 'cli.analytics', false.toString()],
-    {
-      cwd: opts.appDir,
-      all: true,
-      env: { FORCE_COLOR: true.toString() },
-    },
-  )
-  Log.stream(disableAnalyticsCommand.all)
-  await disableAnalyticsCommand
+  return execa(opts.cliBinary, ['config', 'cli.analytics', false.toString()], {
+    cwd: opts.appDir,
+  })
 }

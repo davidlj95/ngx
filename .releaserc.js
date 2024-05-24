9626cdb549f3372a76da4d5e74065b3d688a96f8
David LJ <mail@davidlj95.com>
2024-05-24 16:54:03 +0200

    Revert "build: use latest as npm dist tag"
    This reverts commit 7926afa5e4b0cbccab167055443935479a1eb112.

--- .releaserc.js
+++ .releaserc.js
@@ -6,7 +6,7 @@ module.exports = {
     //👇 Fake branch so that we can release beta versions in `main`
     //   until we can release 1.0.0
     'semantic-release',
-    { name: 'main', prerelease: 'beta', channel: 'latest' },
+    { name: 'main', prerelease: 'beta' },
   ],
   plugins: [
     '@semantic-release/commit-analyzer',

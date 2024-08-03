fa43608b167b759753bc22843c13e628fbb632f0
David LJ <mail@davidlj95.com>
2024-08-03 16:00:01 +0200

    build: revert dead code elimination hack for coverage

--- instrument-for-coverage.sh
+++ instrument-for-coverage.sh
@@ -9,9 +9,3 @@ NGX_META_DIST_DIR="projects/ngx-meta/dist/fesm2022"
 echo "ℹ️ Instrumenting for coverage with 'nyc'"
 echo "     - Directory: '$NGX_META_DIST_DIR'"
 nyc instrument "$NGX_META_DIST_DIR" --in-place
-
-echo "ℹ️ Replacing 'ngDevMode' plain usages to avoid tree-shaking"
-BACKUP_EXT=".bak"
-find "$NGX_META_DIST_DIR" -type f -name '*.*js' -exec \
-  sed -i"$BACKUP_EXT" 's/ngDevMode/globalThis.ngDevMode/g' {} \;
-rm -rf "$NGX_META_DIST_DIR"/*"${BACKUP_EXT:?}"

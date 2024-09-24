5befe1e8a7183c3f33ab81dedbb8b6e61718ab00
David LJ <mail@davidlj95.com>
2024-09-24 15:53:33 +0200

    test: improve ngx meta core module unit tests (#829)

--- projects/ngx-meta/src/core/src/ngx-meta-core.module.spec.ts
+++ projects/ngx-meta/src/core/src/ngx-meta-core.module.spec.ts
@@ -1,15 +1,32 @@
 import { TestBed } from '@angular/core/testing'
 import { NgxMetaCoreModule } from './ngx-meta-core.module'
 import { DEFAULTS_TOKEN } from './defaults-token'
+import { ModuleWithProviders } from '@angular/core'
+import { GlobalMetadata } from '@davidlj95/ngx-meta/core'
 
 describe('Core module', () => {
   it('provides no defaults by default', () => {
-    TestBed.configureTestingModule({ imports: [NgxMetaCoreModule.forRoot()] })
+    makeSut(NgxMetaCoreModule.forRoot())
     expect(TestBed.inject(DEFAULTS_TOKEN, null, { optional: true })).toBeNull()
   })
 
-  it('provides no defaults if not specified', () => {
-    TestBed.configureTestingModule({ imports: [NgxMetaCoreModule.forRoot({})] })
+  it('provides no defaults if options object is empty', () => {
+    makeSut(NgxMetaCoreModule.forRoot({}))
     expect(TestBed.inject(DEFAULTS_TOKEN, null, { optional: true })).toBeNull()
   })
+
+  it('provides defaults if specified as options object', () => {
+    const defaults: GlobalMetadata = { title: 'Hello World!' }
+    makeSut(NgxMetaCoreModule.forRoot({ defaults }))
+
+    expect(TestBed.inject(DEFAULTS_TOKEN, null, { optional: true })).toEqual(
+      defaults,
+    )
+  })
 })
+
+const makeSut = (
+  moduleWithProviders: ModuleWithProviders<NgxMetaCoreModule>,
+) => {
+  TestBed.configureTestingModule({ imports: [moduleWithProviders] })
+}

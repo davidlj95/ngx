ecf6cf3373943f0ab0c2085c725443217be2a2f5
David LJ <mail@davidlj95.com>
2024-10-07 12:52:34 +0200

    chore: try removing an exports layer

--- projects/ngx-meta/src/core/src/meta-elements/index.ts
+++ projects/ngx-meta/src/core/src/meta-elements/index.ts
@@ -9,4 +9,18 @@ export {
 export { NgxMetaMetaService } from './ngx-meta-meta.service'
 export { NgxMetaMetaContent } from './ngx-meta-meta-content'
 export { NgxMetaMetaDefinition } from './ngx-meta-meta-definition'
-export * from './v2'
+// v2
+export {
+  NGX_META_ELEMENT_SETTER,
+  NgxMetaElementSetter,
+} from './v2/ngx-meta-element-setter'
+export {
+  NGX_META_ELEMENTS_SETTER,
+  NgxMetaElementsSetter,
+} from './v2/ngx-meta-elements-setter'
+export { NgxMetaElementAttributes } from './v2/ngx-meta-element-attributes'
+export {
+  withNameAttribute,
+  withPropertyAttribute,
+  withContentAttribute,
+} from './v2/meta-elements-helpers'

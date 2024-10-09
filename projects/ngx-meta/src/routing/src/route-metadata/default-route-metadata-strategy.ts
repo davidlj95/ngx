66d57292f72ff83e47114a13b5cb62b84968303b
David LJ <mail@davidlj95.com>
2024-10-10 00:04:31 +0200

    refactor: turn default route metadata strategy into provider (#912)

--- projects/ngx-meta/src/routing/src/route-metadata/default-route-metadata-strategy.ts
+++ projects/ngx-meta/src/routing/src/route-metadata/default-route-metadata-strategy.ts
@@ -1,24 +1,16 @@
-import { inject, InjectionToken } from '@angular/core'
+import { FactoryProvider } from '@angular/core'
 import { ActivatedRoute, ActivatedRouteSnapshot } from '@angular/router'
-import { _RouteMetadataStrategy } from '@davidlj95/ngx-meta/core'
+import { _routeMetadataStrategy } from '@davidlj95/ngx-meta/core'
 import { NgxMetaRouteData } from './ngx-meta-route-data'
 
-export const DEFAULT_ROUTE_METADATA_STRATEGY =
-  new InjectionToken<_RouteMetadataStrategy>(
-    ngDevMode ? 'NgxMeta Default route metadata strategy' : 'NgxMetaDRMS',
-    {
-      factory: () => {
-        const activatedRoute = inject(ActivatedRoute)
-        return () => {
-          let currentRouteSnapshot: ActivatedRouteSnapshot =
-            activatedRoute.snapshot
-          while (currentRouteSnapshot.firstChild != null) {
-            currentRouteSnapshot = currentRouteSnapshot.firstChild
-          }
-          return currentRouteSnapshot.data[
-            'meta' satisfies keyof NgxMetaRouteData
-          ]
-        }
-      },
-    },
-  )
+export const DEFAULT_ROUTE_METADATA_STRATEGY_PROVIDER: FactoryProvider = {
+  provide: _routeMetadataStrategy(),
+  useFactory: (activatedRoute: ActivatedRoute) => () => {
+    let currentRouteSnapshot: ActivatedRouteSnapshot = activatedRoute.snapshot
+    while (currentRouteSnapshot.firstChild != null) {
+      currentRouteSnapshot = currentRouteSnapshot.firstChild
+    }
+    return currentRouteSnapshot.data['meta' satisfies keyof NgxMetaRouteData]
+  },
+  deps: [ActivatedRoute],
+}

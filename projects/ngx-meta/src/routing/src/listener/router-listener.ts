7eae490651f16cf37df421e80a66c11b5968c84a
David LJ <mail@davidlj95.com>
2024-10-09 17:47:54 +0200

    refactor: use lazy injection token for router listener

--- projects/ngx-meta/src/routing/src/listener/router-listener.ts
+++ projects/ngx-meta/src/routing/src/listener/router-listener.ts
@@ -1,27 +1,29 @@
-import { inject, InjectionToken, OnDestroy } from '@angular/core'
+import { inject, OnDestroy } from '@angular/core'
 import { NavigationEnd, Router } from '@angular/router'
 import { filter, Subscription } from 'rxjs'
-import { _formatDevMessage, NgxMetaService } from '@davidlj95/ngx-meta/core'
+import {
+  _formatDevMessage,
+  _makeInjectionToken,
+  NgxMetaService,
+} from '@davidlj95/ngx-meta/core'
 import { MODULE_NAME } from '../module-name'
 
 // WTF is this? Why not just import `EventType`? Well, compatibility reasons 🙃
 // See https://github.com/davidlj95/ngx/pull/246 for the details
 const NAVIGATION_END_EVENT_TYPE = new NavigationEnd(0, '', '').type
 
-export const ROUTER_LISTENER = new InjectionToken<RouterListener>(
-  ngDevMode ? 'NgxMeta Router listener' : 'NgxMetaRL',
-  {
-    factory: () => {
-      const router = inject(Router)
-      const ngxMetaService = inject(NgxMetaService)
-      let subscription: Subscription | undefined
-      return {
-        listen() {
-          ngDevMode &&
-            subscription &&
-            // https://github.com/istanbuljs/istanbuljs/issues/719
-            // prettier-ignore
-            console.warn(
+export const routerListener = () =>
+  _makeInjectionToken(ngDevMode ? 'Router listener' : 'RL', () => {
+    const router = inject(Router)
+    const ngxMetaService = inject(NgxMetaService)
+    let subscription: Subscription | undefined
+    return {
+      listen() {
+        ngDevMode &&
+          subscription &&
+          // https://github.com/istanbuljs/istanbuljs/issues/719
+          // prettier-ignore
+          console.warn(
               _formatDevMessage(
                 [
                   'prevented listening for route changes twice',
@@ -29,23 +31,21 @@ export const ROUTER_LISTENER = new InjectionToken<RouterListener>(
                 ].join('\n'),
                 { module: MODULE_NAME },
               ))
-          /* istanbul ignore next https://github.com/istanbuljs/istanbuljs/issues/719 */
-          if (subscription) {
-            return
-          }
-          subscription = router.events
-            .pipe(filter(({ type }) => type === NAVIGATION_END_EVENT_TYPE))
-            .subscribe({
-              next: () => ngxMetaService.set(),
-            })
-        },
-        // Can't use DestroyRef is introduced in Angular v16
-        // https://github.com/angular/angular/pull/49158
-        // It uses EnvironmentInjector's internal `onDestroy` under the hood
-        // Tried that too, but it's @internal + raises some errors. So back to the `ngOnDestroy`
-        ngOnDestroy: () => subscription?.unsubscribe(),
-      }
-    },
-  },
-)
+        /* istanbul ignore next https://github.com/istanbuljs/istanbuljs/issues/719 */
+        if (subscription) {
+          return
+        }
+        subscription = router.events
+          .pipe(filter(({ type }) => type === NAVIGATION_END_EVENT_TYPE))
+          .subscribe({
+            next: () => ngxMetaService.set(),
+          })
+      },
+      // Can't use DestroyRef is introduced in Angular v16
+      // https://github.com/angular/angular/pull/49158
+      // It uses EnvironmentInjector's internal `onDestroy` under the hood
+      // Tried that too, but it's @internal + raises some errors. So back to the `ngOnDestroy`
+      ngOnDestroy: () => subscription?.unsubscribe(),
+    }
+  })
 export type RouterListener = { listen: () => void } & OnDestroy

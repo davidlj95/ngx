bf0ffe725db761bcb61fa67e5b73e634e9056958
David LJ <mail@davidlj95.com>
2024-10-10 17:31:43 +0200

    docs: move `http-equiv` docs to parent type

--- projects/ngx-meta/src/core/src/meta-elements/v2/ngx-meta-element-attributes.ts
+++ projects/ngx-meta/src/core/src/meta-elements/v2/ngx-meta-element-attributes.ts
@@ -1,21 +1,18 @@
 /**
  * Models a `<meta>` element HTML's attributes as a key / value map.
  *
- * Inspired on Angular's {@link https://angular.dev/api/platform-browser/MetaDefinition/ | MetaDefinition}
+ * Almost equivalent to Angular's {@link https://angular.dev/api/platform-browser/MetaDefinition/ | MetaDefinition}
  *
- * Only difference is `http-equiv` property.
+ * Only difference is `http-equiv` property. In an Angular's
+ * {@link https://angular.dev/api/platform-browser/MetaDefinition/ | MetaDefinition}, `httpEquiv` would also be
+ * accepted. This way there's no need to quote the key property.
+ * But without `httpEquiv` there's no need to map attribute names. So one bit of code less.
  *
  * @alpha
  */
 export type NgxMetaElementAttributes = Partial<{
   charset: string
   content: string
-  /**
-   * In an Angular's {@link https://angular.dev/api/platform-browser/MetaDefinition/ | MetaDefinition},
-   * `httpEquiv` would also be accepted. This way there's no need to quote the key property.
-   *
-   * But without `httpEquiv` there's no need to map attribute names. So one more bit of code less around.
-   */
   'http-equiv': string
   id: string
   itemprop: string

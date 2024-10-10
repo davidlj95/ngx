efaa1e3ded3e3ecf76aa226f29d1812f85534c8a
David LJ <mail@davidlj95.com>
2024-10-10 20:27:04 +0200

    refactor: move v1 meta element APIs to directory (#917)

--- projects/ngx-meta/src/core/src/meta-elements/index.ts
+++ projects/ngx-meta/src/core/src/meta-elements/index.ts
@@ -1,12 +1,2 @@
-export {
-  makeComposedKeyValMetaDefinition,
-  MakeComposedKeyValMetaDefinitionOptions,
-} from './make-composed-key-val-meta-definition'
-export {
-  makeKeyValMetaDefinition,
-  MakeKeyValMetaDefinitionOptions,
-} from './make-key-val-meta-definition'
-export { NgxMetaMetaService } from './ngx-meta-meta.service'
-export { NgxMetaMetaContent } from './ngx-meta-meta-content'
-export { NgxMetaMetaDefinition } from './ngx-meta-meta-definition'
+export * from './v1'
 export * from './v2'
---
+++ projects/ngx-meta/src/core/src/meta-elements/v1/index.ts
@@ -0,0 +1,11 @@
+export {
+  makeComposedKeyValMetaDefinition,
+  MakeComposedKeyValMetaDefinitionOptions,
+} from './make-composed-key-val-meta-definition'
+export {
+  makeKeyValMetaDefinition,
+  MakeKeyValMetaDefinitionOptions,
+} from './make-key-val-meta-definition'
+export { NgxMetaMetaService } from './ngx-meta-meta.service'
+export { NgxMetaMetaContent } from './ngx-meta-meta-content'
+export { NgxMetaMetaDefinition } from './ngx-meta-meta-definition'

73367a01ad8f108f47994b4ca93c07e0dede5d0a
David LJ <mail@davidlj95.com>
2024-10-19 17:04:58 +0200

    ci: add schematics tests to CI/CD

--- jest.config.ts
+++
@@ -1,7 +0,0 @@
-import { type JestConfigWithTsJest } from 'ts-jest'
-
-const jestConfig: JestConfigWithTsJest = {
-  projects: ['<rootDir>/projects/ngx-meta/schematics'],
-}
-
-export default jestConfig

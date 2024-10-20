ffac42131df15c679ef381c9abed67bc4a1914f0
David LJ <mail@davidlj95.com>
2024-10-20 13:28:43 +0200

    build: add linting for schematics

--- projects/ngx-meta/schematics/ng-add/index.spec.ts
+++ projects/ngx-meta/schematics/ng-add/index.spec.ts
@@ -76,7 +76,7 @@ describe('ng-add schematic', () => {
 
 // https://github.com/FortAwesome/angular-fontawesome/blob/0.15.0/projects/schematics/src/ng-add/index.spec.ts#L107
 // https://github.com/angular/components/blob/18.2.8/src/cdk/schematics/testing/test-app.ts
-export const createTestApp = async (
+const createTestApp = async (
   runner: SchematicTestRunner,
   options: Omit<NgNewSchema, 'version'> & Partial<Pick<NgNewSchema, 'version'>>,
 ) => {
@@ -92,7 +92,7 @@ export const createTestApp = async (
 }
 
 // https://github.com/angular/components/blob/18.2.8/src/cdk/schematics/testing/file-content.ts
-export function getFileContent(tree: Tree, filePath: string): string {
+const getFileContent = (tree: Tree, filePath: string): string => {
   const contentBuffer = tree.read(filePath)
 
   if (!contentBuffer) {

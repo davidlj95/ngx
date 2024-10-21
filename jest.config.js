f03557ec1d4cb22ffab19866bebaa7fc31401af8
David LJ <mail@davidlj95.com>
2024-10-21 15:46:42 +0200

    ci: configure Jest for GitHub Actions reporting (#972)

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,15 @@
+// noinspection NpmUsedModulesInstalled
+const { env } = require('process')
+
 /** @type {import('@jest/types').Config.GlobalConfig} */
 module.exports = {
   projects: ['<rootDir>/projects/ngx-meta/schematics'],
+
+  // https://github.com/jestjs/jest/blob/v29.7.0/docs/Configuration.md#github-actions-reporter
+  reporters: Boolean(env['CI'])
+    ? [['github-actions', { silent: false }], 'summary']
+    : undefined,
+
   coverageDirectory: '<rootDir>/coverage/jest',
   coverageReporters: ['json', 'text'],
   collectCoverageFrom: ['<rootDir>/**/*.ts', '!**/testing/**'],

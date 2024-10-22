92b6a43f405736a735097007fb5017688098d238
David LJ <mail@davidlj95.com>
2024-10-22 17:03:57 +0200

    build: use provider APIs for module-based example apps (#981)

--- projects/ngx-meta/example-apps/templates/module/src/app/app.module.template.ts
+++ projects/ngx-meta/example-apps/templates/module/src/app/app.module.template.ts
@@ -9,19 +9,19 @@ import { AllMetaSetByServiceComponent } from './all-meta-set-by-service/all-meta
 import { AllMetaSetByRouteComponent } from './all-meta-set-by-route/all-meta-set-by-route.component'
 import { MetaSetByRouteAndServiceComponent } from './meta-set-by-route-and-service/meta-set-by-route-and-service.component'
 import {
-  NgxMetaCoreModule,
+  provideNgxMetaCore,
   withNgxMetaBaseUrl,
   withNgxMetaDefaults,
 } from '@davidlj95/ngx-meta/core'
 import DEFAULTS_JSON from '@/e2e/cypress/fixtures/defaults.json'
-import { NgxMetaRoutingModule } from '@davidlj95/ngx-meta/routing'
-import { NgxMetaStandardModule } from '@davidlj95/ngx-meta/standard'
+import { provideNgxMetaRouting } from '@davidlj95/ngx-meta/routing'
+import { provideNgxMetaStandard } from '@davidlj95/ngx-meta/standard'
 import {
-  NgxMetaOpenGraphModule,
-  NgxMetaOpenGraphProfileModule,
+  provideNgxMetaOpenGraph,
+  provideNgxMetaOpenGraphProfile,
 } from '@davidlj95/ngx-meta/open-graph'
-import { NgxMetaTwitterCardModule } from '@davidlj95/ngx-meta/twitter-card'
-import { NgxMetaJsonLdModule } from '@davidlj95/ngx-meta/json-ld'
+import { provideNgxMetaTwitterCard } from '@davidlj95/ngx-meta/twitter-card'
+import { provideNgxMetaJsonLd } from '@davidlj95/ngx-meta/json-ld'
 import { OneMetaSetByServiceComponent } from './one-meta-set-by-service/one-meta-set-by-service.component'
 import { UrlResolutionMetaComponent } from './url-resolution-meta/url-resolution-meta.component'
 import { BASE_URL } from '@/e2e/cypress/fixtures/base-url'
@@ -35,24 +35,19 @@ import { BASE_URL } from '@/e2e/cypress/fixtures/base-url'
     OneMetaSetByServiceComponent,
     UrlResolutionMetaComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    NgForOf,
-    RouterOutlet,
-    JsonPipe,
-    NgxMetaCoreModule.forRoot(
+  imports: [BrowserModule, AppRoutingModule, NgForOf, RouterOutlet, JsonPipe],
+  providers: [
+    provideNgxMetaCore(
       withNgxMetaDefaults(DEFAULTS_JSON),
       withNgxMetaBaseUrl(BASE_URL),
     ),
-    NgxMetaRoutingModule.forRoot(),
-    NgxMetaStandardModule,
-    NgxMetaOpenGraphModule,
-    NgxMetaOpenGraphProfileModule,
-    NgxMetaTwitterCardModule,
-    NgxMetaJsonLdModule,
+    provideNgxMetaRouting(),
+    provideNgxMetaStandard(),
+    provideNgxMetaOpenGraph(),
+    provideNgxMetaOpenGraphProfile(),
+    provideNgxMetaTwitterCard(),
+    provideNgxMetaJsonLd(),
   ],
-  providers: [],
   bootstrap: [AppComponent],
 })
 export class AppModule {}

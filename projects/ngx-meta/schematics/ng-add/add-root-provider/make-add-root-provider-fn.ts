becf28852b7f16e288cc5214ba54002186f87c12
David LJ <mail@davidlj95.com>
2024-10-24 02:21:39 +0200

    feat: improve `ng-add` logging (#988)

--- projects/ngx-meta/schematics/ng-add/add-root-provider/make-add-root-provider-fn.ts
+++ projects/ngx-meta/schematics/ng-add/add-root-provider/make-add-root-provider-fn.ts
@@ -1,21 +1,14 @@
 import { makeV16AddRootProvider } from './make-v16-add-root-provider'
 import { AddRootProvider } from './index'
 import { type addRootProvider } from '@schematics/angular/utility'
-import { noop } from '@angular-devkit/schematics'
 
-export const makeAddRootProviderFn = async (): Promise<AddRootProvider> => {
+export const makeAddRootProviderFn = async (): Promise<
+  AddRootProvider | undefined
+> => {
   const ngAddRootProvider = (await import('@schematics/angular/utility'))
     .addRootProvider as typeof addRootProvider | undefined
-  /* istanbul ignore next https://github.com/istanbuljs/istanbuljs/issues/719 */
   if (!ngAddRootProvider) {
-    // Standalone schematic utils are only available for v16.1 and above.
-    // https://github.com/angular/angular-cli/commit/b14b959901d5a670da0df45e082b8fd4c3392d14
-    const message =
-      'ngx-meta: `ng add` schematics only work for Angular v16.1 and above, sorry :(\n' +
-      "          Please, setup the library manually. Don't worry, it's just a few lines around :)\n" +
-      '          You can find a guide at: https://ngx-meta.dev/get-started/'
-    console.log(message)
-    return noop
+    return
   }
   return makeV16AddRootProvider(ngAddRootProvider)
 }
--- projects/ngx-meta/schematics/ng-add/index.ts
+++ projects/ngx-meta/schematics/ng-add/index.ts
@@ -1,11 +1,25 @@
-import { chain, noop, Rule } from '@angular-devkit/schematics'
+import { chain, noop, Rule, SchematicContext } from '@angular-devkit/schematics'
 import { Schema } from './schema'
 import { makeAddRootProviderFn } from './add-root-provider'
 
 // noinspection JSUnusedGlobalSymbols (actually used in `collection.json`)
 export function ngAdd(options: Schema): Rule {
-  return async () => {
+  return async (_tree, context) => {
     const addRootProvider = await makeAddRootProviderFn()
+    if (!addRootProvider) {
+      // Standalone schematic utils are only available for v16.1 and above.
+      // https://github.com/angular/angular-cli/commit/b14b959901d5a670da0df45e082b8fd4c3392d14
+      context.logger.warn(
+        [
+          'ngx-meta `ng add` schematics only work for Angular v16.1 and above, sorry :(',
+          "Please, setup the library manually. Don't worry, it's just a few lines around :)",
+          'You can find a guide at: https://ngx-meta.dev/get-started/',
+        ].join('\n'),
+      )
+      logLibraryInfo(context.logger)
+      return
+    }
+
     const addRootProviderToProject = (name: string) =>
       addRootProvider({ name, project: options.project })
 
@@ -15,6 +29,21 @@ export function ngAdd(options: Schema): Rule {
       ...options.metadataModules.map((metadataModule) =>
         addRootProviderToProject(metadataModule),
       ),
+      () => {
+        context.logger.info('ngx-meta library was successfully set up 🚀')
+        logLibraryInfo(context.logger)
+      },
     ])
   }
 }
+
+const logLibraryInfo = (logger: SchematicContext['logger']) =>
+  logger.info(
+    [
+      'For more information, you can check out:',
+      ' - Documentation: https://ngx-meta.dev',
+      ' - Repository:    https://github.com/davidlj95/ngx',
+      ' - NPM:           https://www.npmjs.com/package/@davidlj95/ngx-meta',
+      'If you enjoyed using the library, please star its GitHub repository! ⭐️❤️',
+    ].join('\n'),
+  )

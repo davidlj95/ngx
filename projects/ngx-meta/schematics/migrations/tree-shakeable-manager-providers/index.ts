45671a623b6f4fefd79cc70de75cde36652d9ed5
David LJ <mail@davidlj95.com>
2024-10-30 13:08:01 +0100

    fix: properly setup migrations infra
    after testing locally using `ng update`

--- projects/ngx-meta/schematics/migrations/tree-shakeable-manager-providers/index.ts
+++ projects/ngx-meta/schematics/migrations/tree-shakeable-manager-providers/index.ts
@@ -9,6 +9,7 @@ import { applyChanges } from '../../utils/apply-changes'
 // https://github.com/angular/angular/blob/19.0.0-next.11/packages/core/schematics/migrations/explicit-standalone-flag/index.ts
 // https://github.com/angular/components/blob/19.0.0-next.10/src/google-maps/schematics/ng-update/index.ts
 // https://github.com/ngrx/platform/blob/main/modules/store/migrations/18_0_0-beta/index.ts#L20
+// https://github.com/ngxtension/ngxtension-platform/blob/4.0.0/libs/plugin/src/migrations/rename-computeds/rename-computeds.ts
 // Eventually, generated most of it with ChatGPT though :P
 // However, investigated first how folks are doing it around first to see what's the current way to do it
 // noinspection JSUnusedGlobalSymbols

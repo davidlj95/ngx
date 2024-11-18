c398346ce1430b6d9a01e927850e1d79db0f7a43
David LJ <mail@davidlj95.com>
2024-11-18 11:29:04 +0100

    build(lint): remove subdirectory `eslint.config.js`
    add rule manually in main config file

--- eslint.config.js
+++ eslint.config.js
@@ -74,9 +74,15 @@ module.exports = tseslint.config(
       'json-files/sort-package-json': 'error',
     },
   },
-  // If not placed here and running `eslint .` on root repo dir,
-  // the `projects/ngx-meta/eslint.config.js` file isn't picked up
-  // Feature to do so is still experimental
+  // Angular ESLint added an `eslint.config.js` file inside `projects/ngx-meta/src`
+  // However, ESLint configuration files closer to files are not used by ESLint if running it from root directory
+  // Because configuration file resolution is experimental and must be manually enabled:
+  // https://eslint.org/docs/latest/use/configure/configuration-files#experimental-configuration-file-resolution
+  // But WebStorm will use them. So that can lead to different configs used by IDE and in CI/CD. Hence, removing the
+  // extra configuration file to ensure consistency. The file doesn't contain any new rule, so it's safe to do that.
+  // Here's how the file looked like:
+  // https://github.com/angular-eslint/angular-eslint/blob/v18.4.0/packages/schematics/src/utils.ts
+  // Adding here below extra rules for subdirectories then:
   {
     files: ['projects/ngx-meta/src/package.json'],
     rules: {
